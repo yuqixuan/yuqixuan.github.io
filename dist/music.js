c8cb83eab92eb5b0d965804b0259bbb56d6b8435
yqxuan <1486514334@qq.com>
2020-11-02 17:44:37 +0800

    Site updated: 2020-11-02 17:44:30

---
+++ dist/music.js
@@ -0,0 +1,31 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "九九八十一",
+        artist: '双笙/易言/樊棋/南久',
+        url: 'http://link.hhtjim.com/163/409649830.mp3',
+        cover: 'http://p2.music.126.net/0KcMYyKmWR-6eCkR8znePw==/18198016951567516.jpg?param=130y130',
+      },
+      {
+        name: "烟火人间（完整版）（翻自 添儿呗）",
+        artist: '星华',
+        url: 'http://link.hhtjim.com/163/1454037844.mp3',
+        cover: 'http://p2.music.126.net/ImaCUvwBlwOAvw-5dTU_sg==/109951165174937200.jpg?param=130y130',
+      },
+      {
+        name: "【洛天依】小城书院",
+        artist: '双笙/易言/樊棋/南久',
+        url: 'http://link.hhtjim.com/163/481344305.mp3',
+        cover: 'http://p1.music.126.net/bVCjDw3pY97qAByZ8walbw==/109951162915830758.jpg?param=130y130',
+      },
+      {
+        name: "不纯君 - 小棋童的春花小妹",
+        artist: '药匙墨KITORA',
+        url: 'http://link.hhtjim.com/163/483040786.mp3',
+        cover: 'http://p1.music.126.net/frbEiye8CRxdlKNoLfDqeg==/109951162943396488.jpg?param=130y130',
+      },
+    ]
+});
